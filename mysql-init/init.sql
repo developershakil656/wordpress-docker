109ce801c7cbf6a7f6bcfd7d516d2a4113d59966
developershakil656 <71166844+developershakil656@users.noreply.github.com>
2026-03-26 10:12:12 +0600

    mysql-init deleted

--- mysql-init/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress_sitehaat;
-CREATE DATABASE IF NOT EXISTS wordpress_akhlaqfood;
-CREATE DATABASE IF NOT EXISTS wordpress_grocery;
-
-GRANT ALL PRIVILEGES ON wordpress_sitehaat.* TO 'b2buser'@'%';
-GRANT ALL PRIVILEGES ON wordpress_akhlaqfood.* TO 'b2buser'@'%';
-GRANT ALL PRIVILEGES ON wordpress_grocery.* TO 'b2buser'@'%';
-
-FLUSH PRIVILEGES;
