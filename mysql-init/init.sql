6bbb37807a9893eebcebf2e5ca45d8d5cb5e9ddf
developershakil656 <71166844+developershakil656@users.noreply.github.com>
2026-03-26 10:05:43 +0600

    re-make

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS wordpress_sitehaat;
 CREATE DATABASE IF NOT EXISTS wordpress_akhlaqfood;
 CREATE DATABASE IF NOT EXISTS wordpress_grocery;
 
-GRANT ALL PRIVILEGES ON wordpress_sitehaat.* TO 'wpuser'@'%';
-GRANT ALL PRIVILEGES ON wordpress_akhlaqfood.* TO 'wpuser'@'%';
-GRANT ALL PRIVILEGES ON wordpress_grocery.* TO 'wpuser'@'%';
+GRANT ALL PRIVILEGES ON wordpress_sitehaat.* TO 'b2buser'@'%';
+GRANT ALL PRIVILEGES ON wordpress_akhlaqfood.* TO 'b2buser'@'%';
+GRANT ALL PRIVILEGES ON wordpress_grocery.* TO 'b2buser'@'%';
 
 FLUSH PRIVILEGES;
